c759323259b4c44bc29d7e34792f9f2ac4a66baf
Berkay Dönmez <60704910+berkaydonmezs@users.noreply.github.com>
2022-02-01 14:25:54 +0300

    Complete contingency stat notebook

--- utils/verif_.py
+++ utils/verif_.py
@@ -21,3 +21,65 @@ def get_anomaly(data, climatology, anomaly_type, resample_by='mean'):
     data_anom = data.groupby(fr"time.{anomaly_type}") - data_mean
     
     return data_anom
+
+def tab_accuracy(hits, correct_negatives, total):
+    """
+    Calculates accuracy from the contingency table
+    """
+    
+    return np.round((hits+correct_negatives)/total, 3)
+
+def tab_bias_score(hits, false_alarms, misses):
+    """
+    Calculates bias score from the contingency table
+    """
+    return np.round((hits+false_alarms)/(hits+misses), 3)
+
+def tab_POD(hits, misses):
+    """
+    Calculates probability of detection (POD) from the contingency table
+    """
+    
+    return np.round(hits/(hits+misses), 3)
+
+def tab_FAR(hits, false_alarms):
+    """
+    Calculates false alarm ratio (FAR) from the contingency table
+    """
+    
+    return np.round(false_alarms/(hits+false_alarms), 3)
+
+def tab_POFD(correct_negatives, false_alarms):
+    """
+    Calculates probability of false detection (false alarm rate) (POFD) from the contingency table
+    """
+    
+    return np.round(false_alarms/(correct_negatives+false_alarms), 3)
+
+def tab_success_ratio(hits, false_alarms):
+    """
+    Calculates success ratio from the contingency table
+    """
+    
+    return np.round(hits/(hits+false_alarms), 3)
+
+def tab_threat_score(hits, misses, false_alarms):
+    """
+    Calculates threat score from the contingency table
+    """
+    
+    return np.round(hits/(hits+misses+false_alarms), 3)
+
+def tab_equitable_threat_score(hits, hits_random, misses, false_alarms):
+    """
+    Calculates equitable threat score from the contingency table
+    """
+    
+    return np.round((hits-hits_random)/(hits+misses+false_alarms-hits_random), 3)
+
+def tab_HK(hits, correct_negatives, misses, false_alarms):
+    """
+    Calculates Hanssen and Kuipers discriminant (Peirce's skill score) from the contingency table
+    """
+    
+    return np.round((hits/(hits+misses)) - (false_alarms/(false_alarms+correct_negatives)), 3)
